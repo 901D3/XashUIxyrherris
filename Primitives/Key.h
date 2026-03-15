f644b606c97ac68e7beda3e972a278521cdd96c3
🗣🗣: "ùồầỳ hợp lý quãi chất thế nhờ"🤫🗿🔥🔥 <148612010+901D3@users.noreply.github.com>
2026-03-15 16:23:11 +0700

    Clean Key.h

--- Primitives/Key.h
+++ Primitives/Key.h
@@ -1,86 +1,8 @@
 
 #pragma once
 
-#include "engine/keydefs.h"
-
 typedef enum {
   KEY_CONSOLE = 0,
   KEY_GAME,
   KEY_MENU,
 } KeyDestEnums;
-
-namespace KeyUtils {
-  inline bool isEscape(int key) {
-    return key == K_ESCAPE;
-  }
-
-  inline bool isEnter(int key) {
-    return key == K_ENTER;
-  }
-
-  inline bool isBackspace(int key) {
-    return key == K_BACKSPACE;
-  }
-
-  inline bool isHome(int key) {
-    return key == K_HOME;
-  }
-
-  inline bool isEnd(int key) {
-    return key == K_END;
-  }
-
-  inline bool isMouseKeys(int key) {
-    return (
-      key == K_MOUSE1 ||
-      key == K_MOUSE2 ||
-      key == K_MOUSE3 ||
-      key == K_MOUSE4 ||
-      key == K_MOUSE5);
-  }
-
-  inline bool isLeftMouse(int key) {
-    return key == K_MOUSE1;
-  }
-
-  inline bool isUpArrow(int key) {
-    return key == K_KP_UPARROW;
-  }
-
-  inline bool isLeftArrow(int key) {
-    return key == K_KP_LEFTARROW;
-  }
-
-  inline bool isRightArrow(int key) {
-    return key == K_KP_RIGHTARROW;
-  }
-
-  inline bool isDownArrow(int key) {
-    return key == K_KP_DOWNARROW;
-  }
-
-  inline bool isNavigationKeys(int key) {
-    return (
-      key == K_KP_UPARROW ||
-      key == K_KP_LEFTARROW ||
-      key == K_KP_RIGHTARROW ||
-      key == K_KP_DOWNARROW ||
-      key == K_TAB);
-  }
-
-  inline bool isPageUp(int key) {
-    return key == K_PGUP;
-  }
-
-  inline bool isPageDown(int key) {
-    return key == K_PGDN;
-  }
-
-  inline bool isConsoleKey(int key) {
-    return (key == '`' || key == '~');
-  }
-
-  inline bool isAlphaNumericKeys(int key) {
-    return (key >= '0' && key <= '9') || (key >= 'a' && key <= 'z');
-  }
-}
