3cf9692ae1a451587a0759856b3cbc61658fd6ad
🗣🗣: "ùồầỳ hợp lý quãi chất thế nhờ"🤫🗿🔥🔥 <148612010+901D3@users.noreply.github.com>
2026-03-16 12:58:32 +0700

    Removed project dependencies, added ALIGNMENT_CENTER for border sides

--- Primitives/Transform.h
+++ Primitives/Transform.h
@@ -1,27 +1,12 @@
 
 #pragma once
 
-#include "common/xash3d_types.h"
-#include "Base.h"
-
-#include "Primitives/Bitwise.h"
-
-#include "Utils/BitwiseUtils.h"
-
-#define WINDOW_WIDTH (globalVars->scrWidth)
-#define WINDOW_HEIGHT (globalVars->scrHeight)
-
-#define UI_BASE_WIDTH 1024
-#define UI_BASE_HEIGHT 768
-
-#define UI_TARGET_ASPECT_WIDTH 4
-#define UI_TARGET_ASPECT_HEIGHT 3
-
 typedef enum {
-  ALIGNMENT_LEFT = 1,
-  ALIGNMENT_TOP = mcr_bitUint(1),
-  ALIGNMENT_RIGHT = mcr_bitUint(2),
-  ALIGNMENT_BOTTOM = mcr_bitUint(3),
+  ALIGNMENT_CENTER = 0b1111,
+  ALIGNMENT_LEFT = 0b0001,
+  ALIGNMENT_TOP = 0b0010,
+  ALIGNMENT_RIGHT = 0b0100,
+  ALIGNMENT_BOTTOM = 0b1000,
 
   ALIGNMENT_TOP_LEFT = ALIGNMENT_TOP | ALIGNMENT_LEFT,
   ALIGNMENT_TOP_RIGHT = ALIGNMENT_TOP | ALIGNMENT_RIGHT,
